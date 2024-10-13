6b4d962bdc024d57b682c24e046e52db39069ee8
RosettaYH <rosettahu@gmail.com>
2024-10-13 13:03:43 -0400

    Add data versioning reports

--- src/dataversioning/docker-shell.sh
+++ src/dataversioning/docker-shell.sh
@@ -5,9 +5,9 @@ set -e
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
 export GCS_BUCKET_NAME="preppal-data"
-export GCP_PROJECT="preppal"
+export GCP_PROJECT="preppal-438123"
 export GCP_ZONE="us-east1"
-export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/../../../secrets/data-service-account.json"
 
 echo "Building image"
 docker build -t data-version-cli -f Dockerfile .
