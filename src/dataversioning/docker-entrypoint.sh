57be13cc635ccffc61ed15fced0d535912615b85
RosettaYH <rosettahu@gmail.com>
2024-10-18 19:52:58 -0400

    Add DVC history

--- src/dataversioning/docker-entrypoint.sh
+++ src/dataversioning/docker-entrypoint.sh
@@ -10,7 +10,9 @@ fi
 mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/dvc_store
+mkdir -p /app/dvc_store /app/dvc_store/rag_data /app/dvc_store/rag_data/embedded_recipes_entire_recipe /app/dvc_store/rag_data/embedded_recipes_sliding_window
 mount --bind /mnt/gcs_data/rag_data /app/dvc_store
+mount --bind /mnt/gcs_data/rag_data/embedded_recipes_entire_recipe /app/dvc_store/rag_data/embedded_recipes_entire_recipe
+mount --bind /mnt/gcs_data/rag_data/embedded_recipes_sliding_window /app/dvc_store/rag_data/embedded_recipes_sliding_window
 
 pipenv shell
