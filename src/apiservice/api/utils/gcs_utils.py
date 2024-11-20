f2d028ee335a093edf6d7b99b5074b2b1a6b53eb
RosettaYH <rosettahu@gmail.com>
2024-11-20 15:33:44 -0500

    Add __init__.py files

--- src/apiservice/api/utils/gcs_utils.py
+++ src/apiservice/api/utils/gcs_utils.py
@@ -2,21 +2,16 @@ from google.cloud import storage
 import json
 import os
 
-GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "your-gcs-bucket")
-USER_DB_FILE = "user_db.json"
-
-# Initialize GCS client
-storage_client = storage.Client()
-
 
 def get_bucket():
+    GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "your-gcs-bucket")
+    storage_client = storage.Client()
+
     return storage_client.bucket(GCS_BUCKET_NAME)
 
 
 def load_user_db():
-    """
-    Load the user database from GCS.
-    """
+    USER_DB_FILE = "user_db.json"
     bucket = get_bucket()
     blob = bucket.blob(USER_DB_FILE)
     if not blob.exists():
@@ -26,9 +21,7 @@ def load_user_db():
 
 
 def save_user_db(user_db):
-    """
-    Save the updated user database to GCS.
-    """
+    USER_DB_FILE = "user_db.json"
     bucket = get_bucket()
     blob = bucket.blob(USER_DB_FILE)
     blob.upload_from_string(json.dumps(user_db), content_type="application/json")
