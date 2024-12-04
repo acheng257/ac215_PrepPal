cbb304446dfd6d21f667cabcc95b4782343c5be4
Ioana Cristescu <ioanacristescu@dhcp-10-250-74-246.harvard.edu>
2024-12-04 17:52:56 -0500

    Changed frontend to work in production environmnent

--- src/frontend-react/src/services/DataService.js
+++ src/frontend-react/src/services/DataService.js
@@ -1,6 +1,8 @@
 import { BASE_API_URL, uuid } from "./Common";
 import axios from 'axios';
 
+console.log("BASE_API_URL:", BASE_API_URL)
+
 // Create an axios instance with base configuration
 const api = axios.create({
     baseURL: BASE_API_URL
@@ -31,43 +33,43 @@ const DataService = {
         return null;
     },
     GetPantry: async function (user_id) {
-        return await api.get(BASE_API_URL + "/pantry/" + user_id);
+        return await api.get("/pantry/" + user_id);
     },
     GetChats: async function (model, limit) {
-        return await api.get(BASE_API_URL + "/" + model + "/chats?limit=" + limit);
+        return await api.get("/" + model + "/chats?limit=" + limit);
     },
     GetChat: async function (model, chat_id) {
-        return await api.get(BASE_API_URL + "/" + model + "/chats/" + chat_id);
+        return await api.get("/" + model + "/chats/" + chat_id);
     },
     StartChatWithLLM: async function (model, message) {
-        return await api.post(BASE_API_URL + "/" + model + "/chats/", message);
+        return await api.post("/" + model + "/chats", message);
     },
 
     ContinueChatWithLLM: async function (model, chat_id, message) {
-        return await api.post(BASE_API_URL + "/" + model + "/chats/" + chat_id, message);
+        return await api.post("/" + model + "/chats/" + chat_id, message);
     },
     GetChatMessageImage: function (model, image_path) {
         return BASE_API_URL + "/" + model + "/" + image_path;
     },
     GetRecipeRecommendation: async function (body) {
-        return await api.post(BASE_API_URL + "/recipes/get_recs", body);
+        return await api.post("/recipes/get_recs", body);
     },
     GetUserHistoryRecs: async function (user_id) {
-        return await api.get(BASE_API_URL + "/user/" + user_id + "/history/recommendations");
+        return await api.get("/user/" + user_id + "/history/recommendations");
     },
     UpdateUserHistory: async function (user_id, data) {
         console.log("data is ", data)
-        return await api.post(BASE_API_URL + "/user/" + user_id + "/history", data);
+        return await api.post("/user/" + user_id + "/history", data);
     },
     UploadRecipe: async function(data) {
-        return await api.post(BASE_API_URL + "/recipes/upload-recipe", data);
+        return await api.post("/recipes/upload-recipe", data);
     },
     GetUserPreferences: async function(userId) {
-        return await api.get(`${BASE_API_URL}/recipes/user-preferences/${userId}`);
+        return await api.get(`/recipes/user-preferences/${userId}`);
     },
 
     ToggleFavoriteRecipe: async function(userId, recipeName) {
-        return await api.post(`${BASE_API_URL}/recipes/toggle-favorite`, {
+        return await api.post(`/recipes/toggle-favorite`, {
             user_id: userId,
             recipe_title: recipeName
         });
