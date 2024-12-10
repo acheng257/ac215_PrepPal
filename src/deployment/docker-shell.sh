e090b873c288952687e380bb863bd601aa556b16
jonas-raedler <jonas.raedler@googlemail.com>
2024-12-10 15:59:29 -0500

    added workflow to deployment

--- src/deployment/docker-shell.sh
+++ src/deployment/docker-shell.sh
@@ -10,10 +10,10 @@ export SECRETS_DIR=$(pwd)/../../../secrets/
 export GCP_PROJECT="preppal-438123"
 export GCP_ZONE="us-east1-c"
 export GCP_REGION="us-east1"
-export GOOGLE_APPLICATION_CREDENTIALS=/secrets/deployment.json
-# export GCS_BUCKET_NAME="cheese-app-ml-workflow-demo" -> ML WORKFLOW
-# export GCS_SERVICE_ACCOUNT="ml-workflow@ac215-project.iam.gserviceaccount.com" -> ML WORKFLOW
-# export GCS_PACKAGE_URI="gs://cheese-app-trainer-code" -> ML WORKFLOW
+export GOOGLE_APPLICATION_CREDENTIALS="/secrets/deployment.json"
+export GCS_BUCKET_NAME="preppal-data"
+export GCS_SERVICE_ACCOUNT="ml-workflow@preppal-438123.iam.gserviceaccount.com"
+export GCS_PACKAGE_URI="ml-workflow/preppal_trainer_code"
 
 # Build the image based on the Dockerfile
 #docker build -t $IMAGE_NAME -f Dockerfile .
@@ -29,16 +29,13 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$BASE_DIR/../frontend-react":/frontend-react \
 -v "$BASE_DIR/../llm-rag":/llm-rag \
 -v "$BASE_DIR/../postgres-db":/postgres-db \
+-v "$BASE_DIR/../ml-workflow/data-processor":/data-processor \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e USE_GKE_GCLOUD_AUTH_PLUGIN=True \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCP_ZONE=$GCP_ZONE \
 -e GCP_REGION=$GCP_REGION \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+-e GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT \
+-e GCS_PACKAGE_URI=$GCS_PACKAGE_URI \
 $IMAGE_NAME
-
-# -v "$BASE_DIR/../data-collector":/data-collector \ -> ML WORKFLOW
-# -v "$BASE_DIR/../data-processor":/data-processor \ -> ML WORKFLOW
-# -e GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT \
-# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \ -> ML WORKFLOW
-# -e GCS_PACKAGE_URI=$GCS_PACKAGE_URI \ -> ML WORKFLOW
-# -e WANDB_KEY=$WANDB_KEY \ -> ML WORKFLOW
