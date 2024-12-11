0b3978e5f5f205d0021b6ee09b998c58f769a52f
jonas-raedler <jonas.raedler@googlemail.com>
2024-12-10 19:42:27 -0500

    trainer package stuff added

---
+++ src/ml-workflow/model-training/package/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRED_PACKAGES = []
+
+setup(
+    name="preppal-trainer",
+    version="0.0.1",
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    description="PrepPal Trainer Application",
+)
